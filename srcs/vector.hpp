cfe7886e23a2577601317ac91111b9cec6a9e76d
Theo SANNIE <tsannie@e1r11p1.42.fr>
2021-10-19 09:16:39 +0200

    [+] try constructor

--- srcs/vector.hpp
+++ srcs/vector.hpp
@@ -6,7 +6,7 @@
 /*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 09:39:39 by tsannie           #+#    #+#             */
-/*   Updated: 2021/10/18 15:41:44 by tsannie          ###   ########.fr       */
+/*   Updated: 2021/10/19 09:16:21 by tsannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define VECTOR_HPP
 
 #include <memory>
+#include <iostream>
 
 namespace ft
 {
@@ -43,10 +44,19 @@ class Allocator = std::allocator<T>
 
 
 	public:
-		vector( void ) {}
+		explicit vector (const allocator_type& alloc = allocator_type())
+		{
+			this->_alloc = alloc;
+			this->_tab = NULL;
+			this->_size = 0;
+			std::cout << "ok" << std::endl;
+		}
 		~vector();
 
 	private:
+		allocator_type	_alloc;
+		value_type		*_tab;
+		size_type		_size;
 
 };
 
---
+++ test/vector/begin.test.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   begin.test.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/19 09:00:14 by tsannie           #+#    #+#             */
+/*   Updated: 2021/10/19 09:02:15 by tsannie          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "../../srcs/vector.hpp"
+
+int main ()
+{
+	ft::vector<int> myvector;
+
+	/*for (int i=1; i<=5; i++) myvector.push_back(i);
+
+	std::cout << "myvector contains:";
+	for (std::vector<int>::iterator it = myvector.begin() ; it != myvector.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << '\n';
+
+	return 0;*/
+}
