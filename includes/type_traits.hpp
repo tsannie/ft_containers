9137e24b88a2b1b251fb3a054d09d780ff59d39a
user42 <tsannie@student.42.fr>
2021-10-27 00:37:23 +0200

    [+] type traits

--- includes/type_traits.hpp
+++ includes/type_traits.hpp
@@ -1,33 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   string.hpp                                         :+:      :+:    :+:   */
+/*   type_traits.hpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/25 21:19:24 by tsannie           #+#    #+#             */
-/*   Updated: 2021/10/27 00:22:08 by tsannie          ###   ########.fr       */
+/*   Created: 2021/10/26 08:20:23 by tsannie           #+#    #+#             */
+/*   Updated: 2021/10/27 00:22:06 by tsannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STRING_HPP
-# define STRING_HPP
-
-#include <iostream>
-#include <sstream>
-#include <string>
+#ifndef TYPE_TRAITS_HPP
+# define TYPE_TRAITS_HPP
 
 namespace ft
 {
 
-template<typename T>
-std::string	to_string(T str)
-{
-	std::ostringstream	buf;
+template<bool Cond, class T = void>
+struct enable_if {};
 
-	buf << str;
-	return (buf.str());
-}
+template<class T>
+struct enable_if<true, T> { typedef T type; };
+
+template <class T> struct is_integral;
 
 }
 
