59dac7b7c538de17d3ab33cdefa424296fc97ea9
Theo SANNIE <tsannie@e3r5p10.42.fr>
2021-10-27 20:39:39 +0200

    [fix] rev iterators 1/4

--- includes/iterator.hpp
+++ includes/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 16:39:26 by tsannie           #+#    #+#             */
-/*   Updated: 2021/10/27 18:01:27 by tsannie          ###   ########.fr       */
+/*   Updated: 2021/10/27 20:23:49 by tsannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,21 +37,19 @@ private:
 public:
 
 	reverse_iterator(): _it( Iterator() ) {}
-	explicit reverse_iterator( iterator_type it ): _it(it) {}
+	explicit reverse_iterator( iterator_type it ): _it(it - 1) {}
 
 	template <class Iter>
-	reverse_iterator( reverse_iterator<Iter> const & rev_it )
-	{
-		this->_it = rev_it.base();
-	}
+	reverse_iterator( reverse_iterator<Iter> const & rev_it ):
+		_it(rev_it.base()) {}
 
-	iterator_type		base( void ) const { return (this->_it); }
+	iterator_type		base( void ) const { return (this->_it + 1); }
 
 	reference			operator*( void ) const { return (*this->_it); }
 
 	reverse_iterator	operator+( difference_type n ) const
 	{
-		return (vecIterator(this->_it - n));
+		return (reverse_iterator(this->_it - n + 1));
 	}
 
 	reverse_iterator&	operator++( void )
@@ -62,7 +60,7 @@ public:
 
 	reverse_iterator	operator++( int )
 	{
-		iterator_type	ret(*this);
+		reverse_iterator	ret(*this);
 		this->_it--;
 		return (ret);
 	}
@@ -75,7 +73,7 @@ public:
 
 	reverse_iterator	operator-( difference_type n ) const
 	{
-		return (vecIterator(this->_it + n));
+		return (reverse_iterator(this->_it + n + 1));
 	}
 
 	reverse_iterator&	operator--( void )
@@ -86,7 +84,7 @@ public:
 
 	reverse_iterator	operator--( int )
 	{
-		iterator_type	ret(*this);
+		reverse_iterator	ret(*this);
 		this->_it++;
 		return (ret);
 	}
