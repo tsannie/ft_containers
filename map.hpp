9026904444c4fbea391a46c35ff94d39452fa7a6
Theo SANNIE <tsannie@e1r2p2.42.fr>
2021-11-10 13:48:50 +0100

    [+] not

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/28 14:51:38 by tsannie           #+#    #+#             */
-/*   Updated: 2021/11/10 12:47:44 by tsannie          ###   ########.fr       */
+/*   Updated: 2021/11/10 13:47:47 by tsannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -111,9 +111,10 @@ public:
 		return (const_iterator(this->_tree.minNode()));
 	}
 
-	pair<iterator,bool>	insert(const value_type& val)
+	void	insert(const value_type& val)
 	{
-
+		this->_tree.insertNode( val );
+		//return (NULL);
 	}
 };
 
--- test/map/rbtree.cpp
+++ test/map/rbtree.cpp
@@ -6,26 +6,27 @@
 /*   By: tsannie <tsannie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 16:14:37 by tsannie           #+#    #+#             */
-/*   Updated: 2021/11/10 12:46:06 by tsannie          ###   ########.fr       */
+/*   Updated: 2021/11/10 13:46:40 by tsannie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/RBTree.hpp"
 #include "../../includes/utility.hpp"
 #include "../../map.hpp"
+#include <map>
 
 int	main(void)
 {
 	ft::map<int, char>	test;
 
 
-	/*test.insertNode( ft::pair<int, char>(10, 'c') );
-	test.insertNode( ft::pair<int, char>(10, 'b') );
-	test.insertNode( ft::pair<int, char>(10, 'd') );
-	test.insertNode( ft::pair<int, char>(10, 'a') );
-	test.insertNode( ft::pair<int, char>(10, 'e') );
+	test.insert( ft::pair<int, char>(10, 'c') );
+	//test.insert( ft::pair<int, char>(10, 'b') );
+	//test.insert( ft::pair<int, char>(10, 'd') );
+	//test.insert( ft::pair<int, char>(10, 'a') );
+	//test.insert( ft::pair<int, char>(10, 'e') );
 
-	ft::RBTree<ft::pair<int, char> >::iterator	it;*/
+	//ft::map<int, char>::iterator	it;
 	//test.deleteNode(2);
 
 	//test.printTree();
